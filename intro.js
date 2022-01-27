dbe6b48d9c060ad5e89dc2ecc51776cd5989e264
Siddhant <siddhantsharma669@gmail.com>
2022-01-28 02:03:26 +0530

    For notes purposes

---
+++ intro.js
@@ -0,0 +1,4 @@
+// npm install request - With the help of a script we could request an website server and extract its html data 
+
+//npm install cheerio - To extract meaning full data from the website we use cheerio
+
