7ac3206e25fecaadc0a2036114cdda00baa02f8e
Siddhant <siddhantsharma669@gmail.com>
2022-01-28 02:14:26 +0530

    Added more details to it

--- intro.js
+++ intro.js
@@ -2,3 +2,4 @@
 
 //npm install cheerio - To extract meaning full data from the website we use cheerio
 
+// Difference between API and web scraping is in web scraping the data which we received is in the raw format and we need to perform various operations on it in order to extract the meaningful data out of it ,whereas in case of an API the data is being available is present in the proper format(JavaScript Object Notation (JSON) ) and we just request the data from the API The data which we receive is following a particular format unlike in the case of web scraping where we filter out the meaningful data Out of all the extracted from data 
