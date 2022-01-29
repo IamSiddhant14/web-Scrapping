91d91c819fca559a14771d676b4a508611c86943
Siddhant <siddhantsharma669@gmail.com>
2022-01-30 01:47:16 +0530

    Added instruction

--- intro.js
+++ intro.js
@@ -1,5 +1,17 @@
-// npm install request - With the help of a script we could request an website server and extract its html data 
+// npm install request - With the help of a script we could request an website server to extract its html data 
 
 //npm install cheerio - To extract meaning full data from the website we use cheerio
 
 // Difference between API and web scraping is in web scraping the data which we received is in the raw format and we need to perform various operations on it in order to extract the meaningful data out of it ,whereas in case of an API the data is being available is present in the proper format(JavaScript Object Notation (JSON) ) and we just request the data from the API The data which we receive is following a particular format unlike in the case of web scraping where we filter out the meaningful data Out of all the extracted from data 
+
+
+//STATUS CODES
+//When we send a request to the serever, then the server in response sends a "responce" to the request along with it , it also sends a status code this is to acknowldge  success or failure of the request
+
+//HOW TO FIND AN ELEMENT/CLASS/DIV USING INSPECT
+
+// ctrl+f - This will show up an search bar at the bottom from where we could search an particular element/div/class
+
+// After entering the element which we are looking for in the search bar we could easily finding the matching result which are stored in an array
+
+
